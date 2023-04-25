f2e3ba15402ceb011fd63a86f0d08032c39a3af0
caiohenr1 <caiohenrique-dev@outlook.com>
2023-04-25 17:57:31 -0300

    Primeiro commit do projeto Ignite Feed

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Header } from "./components/Header/Header"
+import './global.css'
+export const App = () => {
+  return (
+    <div>
+      <h1>Ignite Feed</h1>
+      <Header />
+    </div>
+  )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import styles from '../Header/Header.module.css'
+
+export const Header = () => {
+    return (
+        <div>
+            Header
+        </div>
+    )
+}
