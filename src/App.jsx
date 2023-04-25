6ceb379f47d6a53fdaa2f7b5de909fac774a675f
caiohenr1 <caiohenrique-dev@outlook.com>
2023-04-25 20:33:59 -0300

    finalizada sidebar do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { Header } from "./components/Header/Header"
+// styles 
 import './global.css'
+import styles from './App.module.css'
+
+// components
+import { Header } from "./components/Header/Header"
+import { Sidebar } from './components/Sidebar/Sidebar'
+
 export const App = () => {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nisi totam aut officia debitis nesciunt placeat corporis nobis, magni impedit voluptatum. Quam libero commodi amet similique possimus? Aliquid, illum saepe.</p>
+        </main>
+      </div>
     </div>
   )
 }
